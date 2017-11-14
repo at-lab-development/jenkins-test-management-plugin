e06bbc898ec596e6ef523ec3f56864a3b88041d2
Alena Zubrevich <Alena_Zubrevich@epam.com>
2017-11-14 13:56:35 +0300

    bind TestResult and corresponding color

--- src/main/java/org/jenkinsci/plugins/util/TestResult.java
+++ src/main/java/org/jenkinsci/plugins/util/TestResult.java
@@ -2,15 +2,25 @@ package org.jenkinsci.plugins.util;
 
 public enum TestResult {
 
-    FAILED("Failed"),
-    PASSED("Passed"),
-    BLOCKED("Blocked"),
-    UNTESTED("Untested");
+    FAILED("Failed", JiraFormatter.Color.RED),
+    PASSED("Passed", JiraFormatter.Color.GREEN),
+    BLOCKED("Blocked", JiraFormatter.Color.GRAY),
+    UNTESTED("Untested", JiraFormatter.Color.GRAY);
 
     private String text;
+    private JiraFormatter.Color color;
 
-    TestResult(final String text) {
+    TestResult(final String text, JiraFormatter.Color color) {
         this.text = text;
+        this.color = color;
+    }
+
+    public JiraFormatter.Color getColor() {
+        return color;
+    }
+
+    public static JiraFormatter.Color getColor(String value) {
+        return valueOf(TestResult.class, value.toUpperCase()).getColor();
     }
 
     @Override
